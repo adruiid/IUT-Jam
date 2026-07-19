e0faa0394316c76f1bee22381893f0656a6cce3d
NiazXD <niaz.academic200@gmail.com>
2026-07-19 22:57:47 +0600

    Added cow and chicken

---
+++ Assets/Scripts/AddItemAutomatically.cs
@@ -0,0 +1,21 @@
+using CTI;
+using UnityEngine;
+
+public class AddItemAutomatically : MonoBehaviour
+{
+    [SerializeField] private Items[] items;
+
+    public void SendToPlayer()
+    {
+        foreach(Items containItem in items)
+        {
+            int temp = 0;
+            for(int i=0;i<Random.Range(1, 3); i++)
+            {
+                temp = i;
+                InventoryManager.instance.AddItem(containItem);
+            }
+            PickupPopup.Instance.Show(containItem, temp, transform);
+        }
+    }
+}
